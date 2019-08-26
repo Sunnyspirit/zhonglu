0fc120e4ed5afa75f9c426b3d88dab1fa7610f98
zhonglu <zhonglu0319@163.com>
2019-08-26 17:25:46 +0800

    update fenzhi

--- src/com/zhonglu/test/Test3.java
+++ src/com/zhonglu/test/Test3.java
@@ -9,7 +9,10 @@ public enum Test3 {
 	public static void main(String[] args) {
 		Test3 spring2 = Test3.SPRING;
 		System.out.println(SPRING.equals(spring2));
-		
+		System.out.println("7777777777777777777777777777这是分支");
+		System.out.println(SPRING.equals(spring2));
+		System.out.println(SPRING.equals(spring2));
+
 	}
 
 }
